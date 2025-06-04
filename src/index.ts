38b7c806677e108bb906c7c4742b06a0da786425
newt <hi@newty.dev>
2025-06-04 04:12:57 +0100

    feat: custom formatting for updated timestamp

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,29 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { getFiles } from "./files";
 import Handlebars from "handlebars";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 // create github client
 const octokit = github.getOctokit(core.getInput("github-token"));
 const files = await getFiles(octokit);
 
-// update files
+// provide the updated time
+const tz = core.getInput("timezone");
+Handlebars.registerHelper("updated", (template) => {
+  const formatString =
+    typeof template === "string" ? template : "DD-MM-YYYY HH:mm:ss";
+  return dayjs().tz(tz).format(formatString);
+});
+
 files.map(({ content }) => {
   const template = Handlebars.compile(content);
   const newContent = template({
-    updated: new Date(),
+    yes: "hi",
   });
   console.log(newContent);
 });
