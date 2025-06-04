ae6cb5673349ceb5d0eda9cb8b0ae244d46ec79e
newt <hi@newty.dev>
2025-06-04 03:22:42 +0100

    feat: find allowed files

---
+++ src/files.ts
@@ -0,0 +1,52 @@
+import * as core from "@actions/core";
+import { getOctokit } from "@actions/github";
+import { minimatch } from "minimatch";
+
+type Octokit = ReturnType<typeof getOctokit>;
+
+/**
+ * Get files from the repository based on allowed patterns.
+ */
+export async function getFiles(octokit: Octokit): Promise<string[]> {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const allFiles = await getFilesRecursively(octokit, owner, repo);
+  const patterns = core
+    .getInput("allowed-files")
+    .split(",")
+    .map((p) => p.trim());
+
+  return allFiles.filter((file) => 
+  patterns.some((pattern) => minimatch(file, pattern))
+);
+}
+
+async function getFilesRecursively(
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  path: string = "",
+): Promise<string[]> {
+  const response = await octokit.rest.repos.getContent({ owner, repo, path });
+  const files: string[] = [];
+
+  if (Array.isArray(response.data)) {
+    for (const item of response.data) {
+      switch (item.type) {
+        case "file":
+          files.push(item.path);
+          break;
+        case "dir":
+          const subFiles = await getFilesRecursively(
+            octokit,
+            owner,
+            repo,
+            item.path,
+          );
+          files.push(...subFiles);
+          break;
+      }
+    }
+  }
+
+  return files;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { getFiles } from "./files";
 
-const files = core.getInput("allowed-files");
+// create github client
+const octokit = github.getOctokit(core.getInput("github-token"));
+const files = await getFiles(octokit);
 console.log(files);
